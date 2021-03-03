3ad21844f18ea5dd7ec4f2f91f671d87e101a8fc
João <joaolmdj@gmail.com>
2021-03-03 19:47:20 -0300

    commit Urna Eletônica

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'Vereador',
+        numeros: 5,
+        candidatos: [
+            {
+                numero:'38111',
+                name: 'Fulano De Tal',
+                partido: 'ABC',
+                fotos:[
+                    {url: 'Politico.jpg', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero:'77222',
+                name: 'Beltrana Da Silva',
+                partido: 'DEFG',
+                fotos:[
+                    {url: 'Politico1.jpg', legenda: 'Vereador', small: false} 
+                ]
+            },  
+        ]
+    },
+    {
+        titulo: 'Prefeito',
+        numero: 2,
+        candidatos: [
+            {
+                numero: '99',
+                name: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos:[
+                    {url:'Politico.jpg', legenda: 'Prefeito'},
+                    {url:'Politico1.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+                {
+                numero: '84',
+                name: 'Zulano',
+                partido: 'QWERT',
+                vice: 'Zul',
+                fotos:[
+                    {url:'Politico1.jpg', legenda: 'Prefeito'},
+                    {url:'Politico.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]  
+            },
+        ]
+    }
+];
